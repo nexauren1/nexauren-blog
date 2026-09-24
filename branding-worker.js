350e5ace791f5984396ddabdf23ed2ae2ae080e3
nexauren1 <nexaurenstore@gmail.com>
2026-09-24 05:41:36 +0200

    Ensure new Nexauren brand assets override legacy public assets

--- branding-worker.js
+++ branding-worker.js
@@ -1,8 +1,9 @@
 import app from "./worker-entry.js";
 
-const iconTag = '<link rel="icon" href="/assets/favicon-nexauren.png">';
-const ogTag = '<meta property="og:image" content="/assets/social-preview-nexauren.png">';
-const twitterTag = '<meta name="twitter:image" content="/assets/social-preview-nexauren.png">';
+const iconTag = '<link rel="icon" href="/assets/favicon-nexauren.png?v=20260924">';
+const ogTag = '<meta property="og:image" content="/assets/social-preview-nexauren.png?v=20260924">';
+const twitterTag = '<meta name="twitter:image" content="/assets/social-preview-nexauren.png?v=20260924">';
+const brandImage = '/assets/nexauren-brand.png?v=20260924';
 
 export default {
   async fetch(request, env, ctx) {
@@ -17,6 +18,8 @@ export default {
     html = html.replace(/<link[^>]*rel=["'](?:icon|shortcut icon)["'][^>]*>/gi, iconTag);
     html = html.replace(/<meta[^>]*property=["']og:image["'][^>]*>/gi, ogTag);
     html = html.replace(/<meta[^>]*name=["']twitter:image["'][^>]*>/gi, twitterTag);
+    html = html.replace(/<img([^>]*?)src=["']\/nexauren-story-favicon\.(?:svg|png|ico)(?:\?[^"']*)?["']([^>]*)>/gi, '<img$1src="' + brandImage + '"$2>');
+    html = html.replace(/<link[^>]*rel=["']apple-touch-icon["'][^>]*>/gi, '<link rel="apple-touch-icon" href="' + brandImage + '">');
 
     if (!/<link[^>]*rel=["'](?:icon|shortcut icon)["'][^>]*>/i.test(html)) html = html.replace(/<\/head>/i, iconTag + "\n</head>");
     if (!/<meta[^>]*property=["']og:image["'][^>]*>/i.test(html)) html = html.replace(/<\/head>/i, ogTag + "\n</head>");
