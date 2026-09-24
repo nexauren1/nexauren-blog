11b93c597bf18f22a8f6b5ad5dc39b2ba9a718f9
nexauren1 <nexaurenstore@gmail.com>
2026-09-24 05:36:07 +0200

    Add Nexauren branding worker wrapper

--- branding-worker.js
+++ branding-worker.js
@@ -1,2 +1,10 @@
-import app from './worker-entry.js';
-export default app;
+import app from "./worker-entry.js";
+
+export default {
+  async fetch(request, env, ctx) {
+    return app.fetch(request, env, ctx);
+  },
+  async scheduled(controller, env, ctx) {
+    if (typeof app.scheduled === "function") return app.scheduled(controller, env, ctx);
+  }
+};
