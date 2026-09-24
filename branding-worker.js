4805f4e9812dd24ea60faf92ce1988160f7448c5
nexauren1 <nexaurenstore@gmail.com>
2026-09-24 05:36:20 +0200

    Rewrite public branding metadata safely

--- branding-worker.js
+++ branding-worker.js
@@ -15,7 +15,25 @@ export default {
       return response;
     }
 
-    return response;
+    let html = await response.text();
+
+    const oldIcon = '<link rel="icon" href="/nexauren-story-favicon.ico?v=20260922-6"><link rel="icon" type="image/svg+xml" href="/nexauren-story-favicon.svg?v=20260922-6"><link rel="icon" type="image/png" sizes="32x32" href="/nexauren-story-favicon.png?v=20260922-6">';
+    html = html.replace(oldIcon, iconTag);
+
+    const oldOg = '<meta property="og:image" content="https://nexaurenstory.com/nexauren-story-social-preview.png?v=20260922-2">';
+    html = html.replace(oldOg, ogTag);
+
+    const oldTwitter = '<meta name="twitter:image" content="https://nexaurenstory.com/nexauren-story-social-preview.png?v=20260922-2">';
+    html = html.replace(oldTwitter, twitterTag);
+
+    const headers = new Headers(response.headers);
+    headers.delete("content-length");
+
+    return new Response(html, {
+      status: response.status,
+      statusText: response.statusText,
+      headers
+    });
   },
   async scheduled(controller, env, ctx) {
     if (typeof app.scheduled === "function") return app.scheduled(controller, env, ctx);
